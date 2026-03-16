65e17a1a402666fc6c2b9675adf4cc64bc108537
psol <>
2026-03-16 20:17:11 +0000

    cli
    FossilOrigin-Name: 6d0b812365a3a09bc032c88d2374e40e3f355b554d69fd3ca25e4061f577458b

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,43 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main (main) where
 
 import qualified Markov as M
+import qualified Image as I
 import System.Random.Stateful as R
+import Data.List (intercalate)
+import System.Environment
+import System.Exit
+import System.IO
+
+run8Ball :: (R.StatefulGen g IO) => g -> IO ()
+run8Ball gen = do
+    contents <- readFile "samples/8ball.txt"
+    let table = M.buildFreqTable (lines contents)
+    generatedLines <- M.markovGenerate gen table 5
+    print $ unwords generatedLines 
+
+runGenImage :: (R.StatefulGen g IO) => g -> String -> String -> IO ()
+runGenImage gen input output = do
+    contents <- readFile input
+    let table = M.buildFreqTable (lines contents)
+    generated <- M.markovGenerate gen table 50
+    I.writeToFile output generated
+
+runGenText :: (R.StatefulGen g IO) => g -> String -> String -> IO ()
+runGenText gen input output = do
+    contents <- readFile input
+    let table = M.buildFreqTable (lines contents)
+    generated <- M.markovGenerate gen table 50
+    writeFile output $ unwords generated
 
 main :: IO ()
 main = do
-    fortune <- readFile "samples/8ball.txt"
-    let table = M.buildFreqTable (lines fortune)
-    gen <- R.newIOGenM (R.mkStdGen 3)
-    generated <- M.markovGenerate gen table 1
-    print generated
+    args <- getArgs
+    gen  <- R.newStdGen >>= R.newIOGenM
+    case args of 
+        ["image", input, output] -> runGenImage gen input output
+        ["text", input, output]  -> runGenText gen input output
+        ["8ball"] -> run8Ball gen
+        _        -> do 
+            hPutStrLn stderr "usage: [image|text] input_filename output_filename or 8ball"
+            exitWith (ExitFailure 1)
--- readme.md
+++ readme.md
@@ -5,5 +5,4 @@ This one implements a first order Markov chain that I can use to:
     * Generate patterned images with the `JuicyPixels` library
     * Building paragraphs from input text.
 
-![squares](samples/square.png)
 ![map](samples/map2.png)
